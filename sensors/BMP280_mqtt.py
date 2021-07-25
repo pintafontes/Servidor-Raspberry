9998160149b44c41404c297e776ba8917ee1c23b
pintafontes <pintafontes@gmail.com>
2021-07-25 11:58:32 +0200

    Modificado o topic por outro máis xenérico. Eliminadas librerías innecesarias

--- sensors/BMP280_mqtt.py
+++ sensors/BMP280_mqtt.py
@@ -1,10 +1,10 @@
 # O sensor BMP280 ofrece a temperatura e presión atmosférica, e estando debidamente calibrado, ofrece tamén a altitude. Conectase ao porto I2C da Raspberry
 
 import board
-import digitalio
+#import digitalio
 import busio
 import time
-import datetime
+#import datetime
 import paho.mqtt.client as mqtt
 import adafruit_bmp280
 
@@ -19,8 +19,7 @@ bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c, 0x76)
 bmp280.mode = adafruit_bmp280.MODE_FORCE
 
 # change this to match the location's pressure (hPa) at sea level
-bmp280.seaLevelhPa = 1013.25
-#bmp280.seaLevelhPa = 1037.8
+bmp280.seaLevelhPa = 1037.8
 
 # Iniciamos o bucle de toma de datos e comunicacion
 while True:
@@ -35,10 +34,10 @@ while True:
         # Publicamos mensaxes coa temperatura bmp280 e humidade
 
         # Mensaxe de topic simple simple con valor de temperatura
-        client.publish("casa/estudio2/temperatura","{0:0.1f}".format(temperature),qos=0,retain=True)
+        client.publish("casa/salon/temperatura","{0:0.1f}".format(temperature),qos=0,retain=True)
 
         # Mensaxe de topic simple simple con valor de presion
-        client.publish("casa/estudio2/presion","{0:0.1f}".format(pressure),qos=0,retain=True)
+        client.publish("casa/salon/presion","{0:0.1f}".format(pressure),qos=0,retain=True)
 
         # Esperamos 5 minutos para volver executar o script
         time.sleep(300)
